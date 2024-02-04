702dc5ba2fb2ac2cec1e042edb7748ed3b3c74ab
yusuf <yusuferkoc99@gmail.com>
2024-02-04 23:36:08 +0300

    Nav

--- Score Calculator/Views/MainView.swift
+++ Score Calculator/Views/MainView.swift
@@ -13,6 +13,7 @@ struct MainView: View {
             VStack {
                 Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             }
+            .navigationTitle("Sınav Puan Hesaplama")
         }
     }
 }
