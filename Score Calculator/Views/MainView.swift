c19a7a4d186f55b9c3b6df7bbec8a9c2e0c19783
yusuf <yusuferkoc99@gmail.com>
2024-02-04 22:55:59 +0300

    Views

--- Score Calculator/Views/MainView.swift
+++ Score Calculator/Views/MainView.swift
@@ -5,4 +5,14 @@
 //  Created by Yusuf Erkoç on 4.02.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
--- Score Calculator/Views/ResultView.swift
+++ Score Calculator/Views/ResultView.swift
@@ -5,4 +5,14 @@
 //  Created by Yusuf Erkoç on 4.02.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
