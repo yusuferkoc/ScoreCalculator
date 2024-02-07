4a0b032408db50b02770cde362176b0d9b473a41
yusuf <yusuferkoc99@gmail.com>
2024-02-07 23:58:01 +0300

    calc

---
+++ Score Calculator/Views/OrtaogretimView.swift
@@ -0,0 +1,18 @@
+//
+//  OrtaogretimView.swift
+//  Score Calculator
+//
+//  Created by Yusuf Erkoç on 7.02.2024.
+//
+
+import SwiftUI
+
+struct OrtaogretimView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrtaogretimView()
+}
---
+++ Score Calculator/Views/SharedViews/HesaplaButton.swift
@@ -0,0 +1,18 @@
+//
+//  HesaplaButton.swift
+//  Score Calculator
+//
+//  Created by Yusuf Erkoç on 7.02.2024.
+//
+
+import SwiftUI
+
+struct HesaplaButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HesaplaButton()
+}
