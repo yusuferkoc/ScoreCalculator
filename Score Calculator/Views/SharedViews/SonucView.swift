afc81df4fedfece36e882af69bbbbb24b21e257e
yusuf <yusuferkoc99@gmail.com>
2024-02-08 14:34:54 +0300

    sheet

---
+++ Score Calculator/Views/SharedViews/SonucView.swift
@@ -0,0 +1,18 @@
+//
+//  SonucView.swift
+//  Score Calculator
+//
+//  Created by Yusuf Erkoç on 8.02.2024.
+//
+
+import SwiftUI
+
+struct SonucView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SonucView()
+}
