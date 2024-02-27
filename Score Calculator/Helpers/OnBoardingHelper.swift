52a727f2f0e28eb7d746b4dd9e55b61e28d118e3
yusuf <yusuferkoc99@gmail.com>
2024-02-27 18:50:10 +0300

    onboarding

---
+++ Score Calculator/Helpers/OnBoardingHelper.swift
@@ -0,0 +1,8 @@
+//
+//  OnBoardingHelper.swift
+//  Score Calculator
+//
+//  Created by Yusuf Erkoç on 27.02.2024.
+//
+
+import Foundation
---
+++ Score Calculator/Helpers/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  Score Calculator
+//
+//  Created by Yusuf Erkoç on 27.02.2024.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
